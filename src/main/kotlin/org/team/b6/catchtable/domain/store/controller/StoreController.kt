c8695380b59aeadddb2e1b55eac3ee5117415ee6
hunzz <hunzz.study@gmail.com>
2024-01-22 23:47:36 +0900

    [Feat] Store 엔티티 관련 기본 세팅
    - StoreRepository, StoreService, StoreController 클래스 생성 후 의존성 주입
    - StoreRequest 클래스 생성 후 to 메서드 생성
    - StoreResponse 클래스 생성 후 from 메서드 생성

---
+++ src/main/kotlin/org/team/b6/catchtable/domain/store/controller/StoreController.kt
@@ -0,0 +1,9 @@
+package org.team.b6.catchtable.domain.store.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/stores")
+class StoreController {
+}
