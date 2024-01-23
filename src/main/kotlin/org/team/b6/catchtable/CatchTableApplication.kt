8376cfd2a61ce49a5f3bb8532456ddc0714da658
Mhun9799 <muhun1030@gmail.com>
2024-01-23 14:32:52 +0900

    fix : CatchTableApplication 클래스에 누락된 EnableJpaAuditing 어노테이션 추가

--- src/main/kotlin/org/team/b6/catchtable/CatchTableApplication.kt
+++ src/main/kotlin/org/team/b6/catchtable/CatchTableApplication.kt
@@ -2,8 +2,10 @@ package org.team.b6.catchtable
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class CatchTableApplication
 
 fun main(args: Array<String>) {
