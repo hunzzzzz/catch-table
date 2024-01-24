8eef89a42b2c2e77b2fb826e863bef38102e4367
hunzzzzz <hunzz.study@gmail.com>
2024-01-24 11:43:20 +0900

    Refactor: Store 도메인 관련 클래스 리팩토링
    - StoreService 인터페이스, StoreServiceImpl 클래스 제거
    - StoreService 클래스 생성
    - 기타 코드 리팩토링 (주석, 들여쓰기 조정 등)

--- src/main/kotlin/org/team/b6/catchtable/domain/store/service/StoreService.kt
+++ src/main/kotlin/org/team/b6/catchtable/domain/store/service/StoreService.kt
@@ -1,8 +1,11 @@
 package org.team.b6.catchtable.domain.store.service
 
 import org.springframework.data.domain.Sort
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.team.b6.catchtable.domain.store.dto.request.CreateStoreRequest
+import org.team.b6.catchtable.domain.store.dto.request.UpdateStoreRequest
 import org.team.b6.catchtable.domain.store.dto.response.StoreResponse
 import org.team.b6.catchtable.domain.store.model.StoreCategory
 import org.team.b6.catchtable.domain.store.repository.StoreRepository
@@ -13,17 +16,35 @@ import org.team.b6.catchtable.global.exception.InvalidStoreSearchingValuesExcept
 class StoreService(
     private val storeRepository: StoreRepository
 ) {
+    // 카테고리 별 식당 전체 조회
     fun findAllStoresByCategory(category: String) =
         storeRepository.findAllByCategory(
             category = getCategory(category)
         ).map { StoreResponse.from(it) }
 
+    // 카테고리와 정렬 조건을 사용하여 식당 전체 조회
     fun findAllStoresByCategoryWithSortCriteria(category: String, direction: Sort.Direction, criteria: String) =
         storeRepository.findAllByCategory(
             category = getCategory(category),
             sort = Sort.by(direction, getCriteria(criteria))
         ).map { StoreResponse.from(it) }
 
+    // 식당 단일 조회
+    fun findStore(storeId: Long) =
+        StoreResponse.from(getStore(storeId))
+
+    // 식당 등록
+    fun createStore(request: CreateStoreRequest) =
+        StoreResponse.from(storeRepository.save(request.to()))
+
+    // 식당 수정
+    fun updateStore(storeId: Long, request: UpdateStoreRequest) =
+        getStore(storeId).update(request)
+
+    // 식당 제거
+    fun deleteStore(storeId: Long) = storeRepository.deleteById(storeId)
+
+    // 내부 메서드들
     private fun getCategory(category: String) =
         StoreCategory.entries.firstOrNull { it.name == category.uppercase() }
             ?: throw InvalidStoreSearchingValuesException("category")
@@ -31,4 +52,7 @@ class StoreService(
     private fun getCriteria(criteria: String) =
         arrayListOf("name").firstOrNull { it == criteria } // TODO: 정렬 조건 추후 추가
             ?: throw InvalidStoreSearchingValuesException("criteria")
+
+    private fun getStore(storeId: Long) =
+        storeRepository.findByIdOrNull(storeId) ?: throw Exception("") // TODO: 추후 반영
 }
