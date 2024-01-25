717e7523d6aacd94f499d0c2f3c1ddd650c200be
hunzzzzz <hunzz.study@gmail.com>
2024-01-25 21:09:40 +0900

    [Test] Swagger 테스트를 위한 임시 ADMIN 회원가입 기능 구현
    - AdminController, AdminService에 (임시로) 회원가입 기능 구현 --> 추후 삭제 예정
    - SignupMemberRequest의 to메서드 활성화
    - AdminService 내에서 OWNER의 요청 거절 시, 메일 제목이 제대로 작동하지 않는 버그 일부 수정

--- src/main/kotlin/org/team/b6/catchtable/domain/member/controller/AdminController.kt
+++ src/main/kotlin/org/team/b6/catchtable/domain/member/controller/AdminController.kt
@@ -2,6 +2,7 @@ package org.team.b6.catchtable.domain.member.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.team.b6.catchtable.domain.member.dto.request.SignupMemberRequest
 import org.team.b6.catchtable.domain.member.service.AdminService
 
 @RestController
@@ -20,4 +21,10 @@ class AdminController(
     @PostMapping("/stores/{storeId}/fail")
     fun refuseStoreRequirement(@PathVariable storeId: Long) =
         ResponseEntity.ok().body(adminService.handleRequirement(storeId, false))
+
+    // TODO: 추후 삭제 (테스트용)
+    @PostMapping
+    fun registerAdmin(@RequestBody request: SignupMemberRequest) {
+        ResponseEntity.ok().body(adminService.registerAdmin(request))
+    }
 }
