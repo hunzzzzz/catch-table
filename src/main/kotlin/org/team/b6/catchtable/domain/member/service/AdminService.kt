dcd9bab87fac041615ba7b38409abf1c712a4e2f
hunzzzzz <hunzz.study@gmail.com>
2024-01-25 15:14:17 +0900

    [Feat] 식당의 등록/삭제 요청 시 ADMIN이 해당 요청을 승인한 경우 메일 발송
    - MimeMessage를 활용하여 ADMIN이 OWNER의 요청을 승인한 경우 메일 발송
    - 추후 Store과 Member의 관계 설정(혹은 컬럼 변경) 이후 대상 이메일 설정 필요

--- src/main/kotlin/org/team/b6/catchtable/domain/member/service/AdminService.kt
+++ src/main/kotlin/org/team/b6/catchtable/domain/member/service/AdminService.kt
@@ -2,11 +2,13 @@ package org.team.b6.catchtable.domain.member.service
 
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.team.b6.catchtable.domain.store.model.StoreStatus
 import org.team.b6.catchtable.domain.store.repository.StoreRepository
 import org.team.b6.catchtable.global.exception.ModelNotFoundException
+import org.team.b6.catchtable.global.variable.Variables
 
 @Service
 @Transactional
@@ -16,38 +18,47 @@ class AdminService(
 ) {
     fun findAllStoreRequirements() =
         storeRepository.findAll()
-            .filter { unavailableToOrder(it.status) }
+            .filter { unavailableToReservation(it.status) }
 
-    fun accept(storeId: Long) {
+    // TODO : 추후 코드 리팩토링 필요
+    fun accept(storeId: Long) =
         getStore(storeId).let {
             when (it.status) {
-                StoreStatus.WAITING_FOR_CREATE -> it.updateStatus(StoreStatus.OK)
-                StoreStatus.WAITING_FOR_DELETE -> it.updateForDelete()
-                else -> ""
+                StoreStatus.WAITING_FOR_CREATE -> {
+                    sendMail(
+                        email = "", // TODO : 추후 대상 이메일 설정
+                        text = Variables.MAIL_CONTENT_STORE_CREATE_ACCEPTED,
+                        isAccepted = true
+                    )
+                    it.updateStatus(StoreStatus.OK)
+                }
+
+                StoreStatus.WAITING_FOR_DELETE -> {
+                    sendMail(
+                        email = "",
+                        text = Variables.MAIL_CONTENT_STORE_DELETE_ACCEPTED,
+                        isAccepted = true
+                    )
+                    it.updateForDelete()
+                }
+
+                else -> {}
             }
         }
-    }
 
     private fun getStore(storeId: Long) =
         (storeRepository.findByIdOrNull(storeId) ?: throw ModelNotFoundException("식당"))
 
-//    private fun sendMail(email: String, requirement: StoreRequirementCategory, isAccepted: Boolean) {
-//        SimpleMailMessage().let {
-//            it.replyTo = email
-//            it.subject = Variables.MAIL_SUBJECT
-//
-//            if (isAccepted) {
-//                when (requirement) {
-//                    StoreRequirementCategory.CREATE -> it.text = Variables.MAIL_CONTENT_STORE_CREATE_ACCEPTED
-//                    StoreRequirementCategory.UPDATE -> it.text = Variables.MAIL_CONTENT_STORE_UPDATE_ACCEPTED
-//                    StoreRequirementCategory.DELETE -> it.text = Variables.MAIL_CONTENT_STORE_DELETE_ACCEPTED
-//                }
-//            }
-//
-//            javaMailSender.send(it)
-//        }
-//    }
+    private fun sendMail(email: String, text: String, isAccepted: Boolean) {
+        val mimeMessage = javaMailSender.createMimeMessage()
+        val helper = MimeMessageHelper(mimeMessage, false)
+        helper.setTo(email)
+        helper.setSubject(Variables.MAIL_SUBJECT)
+        helper.setText(text)
+        javaMailSender.send(mimeMessage)
+    }
 
-    private fun unavailableToOrder(status: StoreStatus) =
-        (status == StoreStatus.WAITING_FOR_CREATE) || (status == StoreStatus.WAITING_FOR_DELETE)
+
+    private fun unavailableToReservation(status: StoreStatus) =
+        (status == StoreStatus.WAITING_FOR_CREATE) || (status == StoreStatus.WAITING_FOR_DELETE) || (status == StoreStatus.DELETED)
 }
