bd74eff91c86aca0fc0a9976a41ef9fd4bba9832
kDAJIN <152415476+kDAJIN@users.noreply.github.com>
2024-01-26 11:32:18 +0900

    AdminService,AdminController 오류 수정

--- src/main/kotlin/org/team/b6/catchtable/domain/member/service/AdminService.kt
+++ src/main/kotlin/org/team/b6/catchtable/domain/member/service/AdminService.kt
@@ -1,5 +1,6 @@
 package org.team.b6.catchtable.domain.member.service
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.security.access.prepost.PreAuthorize
@@ -8,7 +9,10 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.team.b6.catchtable.domain.member.dto.request.SignupMemberRequest
 import org.team.b6.catchtable.domain.member.repository.MemberRepository
+import org.team.b6.catchtable.domain.review.repository.ReviewRepository
 import org.team.b6.catchtable.domain.store.model.StoreStatus
+import org.team.b6.catchtable.domain.store.repository.StoreRepository
+import org.team.b6.catchtable.global.exception.ModelNotFoundException
 import org.team.b6.catchtable.global.service.GlobalService
 import org.team.b6.catchtable.global.variable.Variables
 
@@ -18,7 +22,9 @@ class AdminService(
     private val globalService: GlobalService,
     private val javaMailSender: JavaMailSender,
     private val memberRepository: MemberRepository,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
+    private val reviewRepository: ReviewRepository,
+    private val storeRepository: StoreRepository
 ) {
     @PreAuthorize("hasRole('ADMIN')")
     fun findAllStoreRequirements() =
@@ -71,15 +77,6 @@ class AdminService(
 
     private fun getMember(memberId: Long) =
         memberRepository.findByIdOrNull(memberId) ?: throw ModelNotFoundException("멤버")
-
-    private fun sendMail(email: String, text: String, isAccepted: Boolean) {
-        val mimeMessage = javaMailSender.createMimeMessage()
-        val helper = MimeMessageHelper(mimeMessage, false)
-        helper.setTo(email)
-        helper.setSubject(Variables.MAIL_SUBJECT)
-        helper.setText(text)
-        javaMailSender.send(mimeMessage)
-    }
 
     private fun sendMail(email: String, subject: String, text: String) =
         javaMailSender.createMimeMessage().let {
