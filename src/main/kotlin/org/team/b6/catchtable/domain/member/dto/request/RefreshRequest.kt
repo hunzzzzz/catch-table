4a317979e2173017155311476ba52dffcb9f3688
Mhun9799 <muhun1030@gmail.com>
2024-01-28 19:07:26 +0900

    [Feat] : logout 기능을 위한 refreshToken 발급, 재발급 기능 추가

---
+++ src/main/kotlin/org/team/b6/catchtable/domain/member/dto/request/RefreshRequest.kt
@@ -0,0 +1,5 @@
+package org.team.b6.catchtable.domain.member.dto.request
+
+data class RefreshRequest(
+    val refreshToken: String,
+)
--- src/main/kotlin/org/team/b6/catchtable/domain/member/dto/response/LoginResponse.kt
+++ src/main/kotlin/org/team/b6/catchtable/domain/member/dto/response/LoginResponse.kt
@@ -1,7 +1,6 @@
 package org.team.b6.catchtable.domain.member.dto.response
 
-import org.team.b6.catchtable.domain.member.dto.request.SignupMemberRequest
-
 data class LoginResponse(
     val accessToken: String,
+    val refreshToken: String,
 )
